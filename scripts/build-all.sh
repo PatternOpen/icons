6072664c7723ab47408abcf34ffbca35119b6c62
Mario34 <zhl.mario34@outlook.com>
2022-04-28 20:29:15 +0800

    chore: update script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,7 +1,3 @@
-pnpm generate:icons
-pnpm -F icon-vue generate
 pnpm -F icon-vue build
-pnpm -F icon-react generate
 pnpm -F icon-react build
-pnpm -F icon-svg generate
 pnpm -F icon-svg build
---
+++ scripts/generate-all.sh
@@ -0,0 +1,4 @@
+pnpm generate:icons
+pnpm -F icon-vue generate
+pnpm -F icon-react generate
+pnpm -F icon-svg generate
